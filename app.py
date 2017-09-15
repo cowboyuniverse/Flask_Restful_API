9ccc341c988a67d9f2fe78355072f8dfb4e56135
cowboyuniverse <cowboyuniverse@gmail.com>
2017-09-14 21:49:25 -0700

    added routes for task_id

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -17,6 +17,14 @@ tasks = [
         'done': False
     }
 ]
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
